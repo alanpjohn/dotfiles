a34794a975525f2d5be7d242a03448982f6817d7
Alan P John <alanpjohn@outlook.com>
2026-03-24 23:31:03 +0530

    feat: Configure efm language server for Go formatting and linting
    Signed-off-by: Alan P John <alanpjohn@outlook.com>

--- init.lua
+++ init.lua
@@ -450,11 +450,41 @@ require('lazy').setup {
                 capabilities = vim.tbl_deep_extend('force', capabilities,
                     require('cmp_nvim_lsp').default_capabilities(capabilities))
 
+                local function ensure_python_tools()
+                    if vim.bo.filetype ~= 'python' then return end
+                    if vim.fn.executable('uv') ~= 1 then return end
+
+                    local tools = { 'pyrefly', 'ruff' }
+                    for _, tool in ipairs(tools) do
+                        local out = vim.fn.system('uv tool list')
+                        if not string.find(out, '^' .. tool .. '$', 1, true) then
+                            vim.fn.system('uv tool install ' .. tool)
+                        end
+                    end
+                end
+
+                ensure_python_tools()
+
                 local servers = {
                     clangd = {},
                     pyrefly = {},
                     ruff = {},
                     gopls = {},
+                    efm = {
+                        init_options = {
+                            documentFormatting = true,
+                            documentRangeFormatting = true,
+                        },
+                        settings = {
+                            rootMarkers = { ".git" },
+                            languages = {
+                                go = {
+                                    { formatCommand = "goimports",       formatStdin = true },
+                                    { lintCommand = "golangci-lint run", lintDebounce = 500 },
+                                },
+                            },
+                        },
+                    },
                     yamlls = {
                         cmd = {
                             "bun",
@@ -536,7 +566,10 @@ require('lazy').setup {
 
                 local ensure_installed = vim.tbl_keys(servers or {})
                 vim.list_extend(ensure_installed, {
-                    'stylua', -- Used to format Lua code
+                    'stylua',        -- Used to format Lua code
+                    'golangci-lint', -- Linter aggregator
+                    'goimports',     -- Formatter with import organization
+                    'golines',       -- Line formatter
                 })
                 require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -551,53 +584,53 @@ require('lazy').setup {
                 end
             end
         },
-        {
-            'dmtrKovalenko/fff.nvim',
-            build = function()
-                -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-                -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-                require("fff.download").download_or_build_binary()
-            end,
-            -- if you are using nixos
-            -- build = "nix run .#release",
-            opts = {                    -- (optional)
-                debug = {
-                    enabled = true,     -- we expect your collaboration at least during the beta
-                    show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
-                },
-            },
-            -- No need to lazy-load with lazy.nvim.
-            -- This plugin initializes itself lazily.
-            lazy = false,
-            keys = {
-                {
-                    "ff", -- try it if you didn't it is a banger keybinding for a picker
-                    function() require('fff').find_files() end,
-                    desc = 'FFFind files',
-                },
-                {
-                    "fg",
-                    function() require('fff').live_grep() end,
-                    desc = 'LiFFFe grep',
-                },
-                {
-                    "fz",
-                    function()
-                        require('fff').live_grep({
-                            grep = {
-                                modes = { 'fuzzy', 'plain' }
-                            }
-                        })
-                    end,
-                    desc = 'Live fffuzy grep',
-                },
-                {
-                    "fc",
-                    function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end,
-                    desc = 'Search current word',
-                },
-            }
-        },
+        -- {
+        --     'dmtrKovalenko/fff.nvim',
+        --     build = function()
+        --         -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+        --         -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+        --         require("fff.download").download_or_build_binary()
+        --     end,
+        --     -- if you are using nixos
+        --     -- build = "nix run .#release",
+        --     opts = {                    -- (optional)
+        --         debug = {
+        --             enabled = true,     -- we expect your collaboration at least during the beta
+        --             show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+        --         },
+        --     },
+        --     -- No need to lazy-load with lazy.nvim.
+        --     -- This plugin initializes itself lazily.
+        --     lazy = false,
+        --     keys = {
+        --         {
+        --             "ff", -- try it if you didn't it is a banger keybinding for a picker
+        --             function() require('fff').find_files() end,
+        --             desc = 'FFFind files',
+        --         },
+        --         {
+        --             "fg",
+        --             function() require('fff').live_grep() end,
+        --             desc = 'LiFFFe grep',
+        --         },
+        --         {
+        --             "fz",
+        --             function()
+        --                 require('fff').live_grep({
+        --                     grep = {
+        --                         modes = { 'fuzzy', 'plain' }
+        --                     }
+        --                 })
+        --             end,
+        --             desc = 'Live fffuzy grep',
+        --         },
+        --         {
+        --             "fc",
+        --             function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end,
+        --             desc = 'Search current word',
+        --         },
+        --     }
+        -- },
         { -- Status Line
             'nvim-lualine/lualine.nvim',
             dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -639,6 +672,8 @@ require('lazy').setup {
                 end,
                 formatters_by_ft = {
                     lua = { 'stylua' },
+                    -- go uses efm-langserver with goimports for LSP formatting
+                    -- python = { 'ruff' },  -- Use LSP formatting instead
                     -- Conform can also run multiple formatters sequentially
                     -- python = { "isort", "black" },
                     --
@@ -668,12 +703,12 @@ require('lazy').setup {
                         -- `friendly-snippets` contains a variety of premade snippets.
                         --    See the README about individual language/framework/plugin snippets:
                         --    https://github.com/rafamadriz/friendly-snippets
-                        -- {
-                        --   'rafamadriz/friendly-snippets',
-                        --   config = function()
-                        --     require('luasnip.loaders.from_vscode').lazy_load()
-                        --   end,
-                        -- },
+                        {
+                            'rafamadriz/friendly-snippets',
+                            config = function()
+                                require('luasnip.loaders.from_vscode').lazy_load()
+                            end,
+                        },
                     },
                 },
                 'saadparwaiz1/cmp_luasnip',
